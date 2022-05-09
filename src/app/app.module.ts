7f9c3108003e8e3c66e8ed5aaaf3f88ac05f7991
bijj2 <bcorrea@mtsolucoes.com.br>
2022-05-09 08:21:55 -0300

    Aula 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { JogadorComponent } from './views/jogador/jogador.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent,
+    JogadorComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
